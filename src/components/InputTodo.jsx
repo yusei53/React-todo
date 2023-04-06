2ea9685391d8b674e3f0338b731951bf1ddee6aa
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-04-06 22:11:39 +0900

    5個以上リストが追加されないように制限する

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -8,11 +8,11 @@ const style = {
 }
 
 export const InputTodo = (props) => {
-  const {todoText, onChange, onClickdd} = props;
+  const {todoText, onChange, onClickdd, disabled} = props;
   return (
         <div style={style}>
-        <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-        <button onClick={onClickdd}>追加</button>
+        <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange} />
+        <button disabled={disabled} onClick={onClickdd}>追加</button>
       </div>
   )
 }
