a53af75364bff140765240d7bfc8050e22292dc3
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-04-06 21:42:11 +0900

    css in jsの導入（一部）

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,16 @@
+const style = { 
+    backgroundColor: "lightblue",
+    width: "700px",
+    padding: "10px",
+    marginBottom: "10px",
+    borderRadius: "10px",
+    alignItems: "center"
+}
+
 export const InputTodo = (props) => {
   const {todoText, onChange, onClickdd} = props;
   return (
-        <div className="input-area">
+        <div style={style}>
         <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
         <button onClick={onClickdd}>追加</button>
       </div>
